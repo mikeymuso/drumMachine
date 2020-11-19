afde12264487c32b722a9ed19a2d32d09bd81dd6
Mike Cotton <mick_cotton@hotmail.com>
2020-11-19 11:38:01 +0000

    fixed some css for Safari

---
+++ detectDevice.js
@@ -0,0 +1,6 @@
+// console.log(navigator.userAgent.match());
+
+// if (navigator.userAgent.match(/Mozilla/i)) {
+// 	console.log('Mozilla');
+// 	document.querySelector('body').innerHTML = '<h1>This app will not work on Mobile Devices</h1>';
+// }
